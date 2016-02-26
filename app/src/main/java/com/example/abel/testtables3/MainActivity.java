f86a00b95c7ea7ec91a119d38a8156bd1ae3f2c3
Abel <email_id>
2016-02-25 22:56:16 -0400

    FIRST COMMIT

---
+++ app/src/main/java/com/example/abel/testtables3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.abel.testtables3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Tabla tabla = new Tabla(this, (TableLayout)findViewById(R.id.tabla));
+        tabla.agregarCabecera(R.array.cabecera_tabla);
+        for(int i = 0; i < 15; i++)
+        {
+            ArrayList<String> elementos = new ArrayList<String>();
+            elementos.add(Integer.toString(i));
+            elementos.add("Casilla [" + i + ", 0]");
+            elementos.add("Casilla [" + i + ", 1]");
+            elementos.add("Casilla [" + i + ", 2]");
+            elementos.add("Casilla [" + i + ", 3]");
+            tabla.agregarFilaTabla(elementos);
+        }
+    }
+}
